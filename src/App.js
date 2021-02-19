da6bbf7073b511ffd55a711f21af5db860bceaec
Emmanuel Losch <emmanuellosch@live.de>
2021-02-19 16:06:31 +0100

    install uuid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
 
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import styled from "styled-components";
+
 function App() {
   return <div></div>;
 }
